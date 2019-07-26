d35d7080e8bfe92d06670fa98741fe0b18429dc4
Stanislav Matas <stanislav.matas@gmail.com>
2019-07-26 08:41:17 +0200

    Refactoring. Helper function for checking presence of list items.

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # tania slysela o husty novy apce to-do
         # jde se na to mrknout
@@ -31,15 +36,12 @@ class NewVisitorTest(unittest.TestCase):
 
         # pise "Nakoupit pavi pera" do textoveho pole (rada vyrabi rybi navnady)
         inputbox.send_keys('Buy peacock feathers')
-
         # kdyz zmackne enter, stranka se obnovi
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         # a stranka ted zobrazi polozku v ocislovanem seznamu
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # stale zde je textove pole rikajici si o pridani dalsi polozky
         # napise "pouzit pavi pera na vyrobu navnady" (Tania je velmi metodicka)
@@ -49,11 +51,14 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # seznam se znovu obnovi a ted Tania vidi obe polozky v seznamu
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly',
-                      [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # Tania se zajima, zda si stranka pamatuje jeji seznam. Pak vidi, ze si pro ni
+        # stranka vygenerovala unikatni URL -- je zde vysvetlujici text k tomuto jevu
+
+        # navstivi tuto URL adresu - jeji to-do seznam tam stale je
+        # uspokojena, jde spat
 
         self.fail('Finish the test!')
 
@@ -61,11 +66,6 @@ class NewVisitorTest(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main()
 
-# stranka se znovu obnovi a ted jsou zobrazeny obe polozky na jejim seznamu
 
-# Tania se zajima, zda si stranka pamatuje jeji seznam. Pak vidi, ze si pro ni
-# stranka vygenerovala unikatni URL -- je zde vysvetlujici text k tomuto jevu
 
-# navstivi tuto URL adresu - jeji to-do seznam tam stale je
 
-# uspokojena, jde spat

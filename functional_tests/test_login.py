872f00640efe8398d0ae53912d5579508cdda982
Stanislav Matas <stanislav.matas@gmail.com>
2019-12-05 05:59:39 +0100

    FT for login via email

---
+++ functional_tests/test_login.py
@@ -0,0 +1,48 @@
+from django.core import mail
+from selenium.webdriver.common.keys import Keys
+import re
+
+from .base import FunctionalTest
+
+TEST_MAIL = 'edith@example.com'
+SUBJECT = 'Your login link for Superlists'
+
+
+class LoginTest(FunctionalTest):
+
+    def test_can_get_email_link_to_log_in(self):
+        # Tania jde stranku a poprve si vsimne sekce "Log in" v navbaru
+        # rika ji to aby zadala svoji emailovou adresu a udela tak
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_name('email').send_keys(TEST_MAIL)
+        self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
+
+        # objevi se zprava ze byl odeslan email
+        self.wait_for(lambda: self.assertIn(
+            'Check your email',
+            self.browser.find_element_by_tag_name('body').text
+        ))
+
+        # zkontroluje svoje emaily a najde zpravu
+        ## Django gives us access to any emails the server tries to send via the mail.outbox attribute
+        email = mail.outbox[0]
+        self.assertIn(TEST_MAIL, email.to)
+        self.assertEqual(email.subject, SUBJECT)
+
+        # ma v sobe URL odkaz
+        self.assertIn('Use this link to log in', email.body)
+        url_search = re.search(r'http://.+/.+$', email.body)
+        if not url_search:
+            self.fail(f'Could not find url in email body:\n{email.body}')
+        url = url_search.group(0)
+        self.assertIn(self.live_server_url, url)
+
+        # klikne na nej
+        self.browser.get(url)
+
+        # prihlasi se!
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text('Log out')
+        )
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(TEST_MAIL, navbar.text)

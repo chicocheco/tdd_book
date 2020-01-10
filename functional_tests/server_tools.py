d7060f5bf671c85bbda854bb89f2b4acf7de2906
Stanislav Matas <stanislav.matas@gmail.com>
2020-01-10 08:10:33 +0100

    add create_session command, add reset_database function to tests and func to get emails on staging

---
+++ functional_tests/server_tools.py
@@ -0,0 +1,29 @@
+from fabric.api import run, env
+from fabric.context_managers import settings, shell_env
+
+env.key_filename = ['~/standa.pem']
+
+
+def _get_manage_dot_py(host):
+    return f'~/sites/{host}/virtualenv/bin/python ~/sites/{host}/manage.py'
+
+
+def reset_database(host):
+    manage_dot_py = _get_manage_dot_py(host)
+    with settings(host_string=f'ubuntu@{host}'):  # TODO: keyfile
+        run(f'{manage_dot_py} flush --noinput')
+
+
+def _get_server_env_vars(host):
+    env_lines = run(f'cat ~/sites/{host}/.env').splitlines()  # get a list
+    # create a generator and pass to a dict() constructor
+    return dict(line.split('=') for line in env_lines if line)
+
+
+def create_session_on_server(host, email):
+    manage_dot_py = _get_manage_dot_py(host)
+    with settings(host_string=f'ubuntu@{host}'):
+        env_vars = _get_server_env_vars(host)
+        with shell_env(**env_vars):  # unpack a dict
+            session_key = run(f'{manage_dot_py} create_session {email}')
+            return session_key.strip()
--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,20 +1,57 @@
+import os
+import poplib
+import time
+import re
+
 from django.core import mail
 from selenium.webdriver.common.keys import Keys
-import re
 
 from .base import FunctionalTest
 
-TEST_MAIL = 'edith@example.com'
 SUBJECT = 'Your login link for Superlists'
 
 
 class LoginTest(FunctionalTest):
 
+    def wait_for_email(self, test_email, subject):
+        if not self.staging_server:
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertEqual(subject, email.subject)
+            return email.body
+
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL('pop.mail.yahoo.com')
+        try:
+            inbox.user(test_email)
+            inbox.pass_(os.environ['YAHOO_PASSWORD'])
+            while time.time() - start < 60:
+                # get 10 newest messages
+                count, _ = inbox.stat()
+                for i in reversed(range(max(1, count - 10), count + 1)):
+                    print('getting msg', i)
+                    _, lines, __ = inbox.retr(i)
+                    lines = [line.decode('utf8') for line in lines]
+                    print(lines)
+                    if f'Subject: {subject}' in lines:
+                        email_id = i
+                        body = '\n'.join(lines)
+                        return body
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+            inbox.quit()
+
     def test_can_get_email_link_to_log_in(self):
         # Tania jde stranku a poprve si vsimne sekce "Log in" v navbaru
         # rika ji to aby zadala svoji emailovou adresu a udela tak
+        if self.staging_server:
+            test_email = 'testuser.2020@yahoo.com'
+        else:
+            test_email = 'tania@example.com'
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_name('email').send_keys(TEST_MAIL)
+        self.browser.find_element_by_name('email').send_keys(test_email)
         self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
 
         # objevi se zprava ze byl odeslan email
@@ -25,15 +62,13 @@ class LoginTest(FunctionalTest):
 
         # zkontroluje svoje emaily a najde zpravu
         ## Django gives us access to any emails the server tries to send via the mail.outbox attribute - mocked out
-        email = mail.outbox[0]
-        self.assertIn(TEST_MAIL, email.to)
-        self.assertEqual(email.subject, SUBJECT)
+        body = self.wait_for_email(test_email, SUBJECT)
 
         # ma v sobe URL odkaz
-        self.assertIn('Use this link to log in', email.body)
-        url_search = re.search(r'http://.+/.+$', email.body)
+        self.assertIn('Use this link to log in', body)
+        url_search = re.search(r'http://.+/.+$', body)
         if not url_search:
-            self.fail(f'Could not find url in email body:\n{email.body}')
+            self.fail(f'Could not find url in email body:\n{body}')
         url = url_search.group(0)
         self.assertIn(self.live_server_url, url)
 
@@ -41,10 +76,11 @@ class LoginTest(FunctionalTest):
         self.browser.get(url)
 
         # prihlasi se!
-        self.wait_to_be_logged_in(email=TEST_MAIL)
+        self.wait_to_be_logged_in(email=test_email)
 
         # ted se odhlasi
         self.browser.find_element_by_link_text('Log out').click()
 
         # je odhlasena
-        self.wait_to_be_logged_out(email=TEST_MAIL)
+        self.wait_to_be_logged_out(email=test_email)
+        # TODO: does 'source .env' do the same as gunicorn .service?

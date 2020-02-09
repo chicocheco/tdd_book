c994114d6ba18854b2d6623b400621f61f0a9317
Stanislav Matas <stanislav.matas@gmail.com>
2020-02-09 19:15:22 +0100

    Add .name attribute to List

--- lists/models.py
+++ lists/models.py
@@ -7,6 +7,10 @@ class List(models.Model):
 
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.CASCADE)
 
+    @property
+    def name(self):
+        return self.item_set.first().text
+
     # can be used in redirect(), instantiated
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
